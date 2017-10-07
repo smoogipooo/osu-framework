af702a37a0a55b7a482619ec497695d344fca0d5
Thomas Müller <thomas94@gmx.net>
2017-10-07 09:57:40 +0200

    Disable depth prepass for SpriteText

--- osu.Framework/Graphics/Sprites/SpriteText.cs
+++ osu.Framework/Graphics/Sprites/SpriteText.cs
@@ -127,6 +127,7 @@ namespace osu.Framework.Graphics.Sprites
         public SpriteText()
         {
             AutoSizeAxes = Axes.Both;
+            ShouldDrawDepth = false;
         }
 
         private const float default_text_size = 20;
