9a70f03c63eea46b8899fa2f0baed0372093499a
smoogipoo <smoogipoo@smgi.me>
2017-12-15 15:50:00 +0900

    Improve testcase display to showcase both raw and markdwon content

--- osu.Framework.Tests/Visual/TestCaseMarkdownRenderer.cs
+++ osu.Framework.Tests/Visual/TestCaseMarkdownRenderer.cs
@@ -10,30 +10,37 @@ using OpenTK.Graphics;
 using osu.Framework.Allocation;
 using osu.Framework.IO.Stores;
 using System.Text;
+using osu.Framework.Graphics.Sprites;
 
 namespace osu.Framework.Tests.Visual
 {
     public class TestCaseMarkdownRenderer : TestCase
     {
-        private readonly MarkdownTextContainer markdownContainer;
+        private readonly DisplayBox markdownBox;
+        private readonly DisplayBox userInputBox;
 
         public TestCaseMarkdownRenderer()
         {
-            Add(new Box
+            Child = new Container
             {
                 RelativeSizeAxes = Axes.Both,
-                Colour = new Color4(50, 50, 50, 255)
-            });
-
-            Add(new ScrollContainer
-            {
-                RelativeSizeAxes = Axes.Both,
-                Child = markdownContainer = new MarkdownTextContainer
+                Padding = new MarginPadding(10),
+                Children = new Drawable[]
                 {
-                    RelativeSizeAxes = Axes.X,
-                    AutoSizeAxes = Axes.Y
+                    userInputBox = new UserTextBox
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                        Width = 0.45f
+                    },
+                    markdownBox = new MarkdownBox
+                    {
+                        Anchor = Anchor.TopRight,
+                        Origin = Anchor.TopRight,
+                        RelativeSizeAxes = Axes.Both,
+                        Width = 0.45f
+                    }
                 }
-            });
+            };
         }
 
         private IResourceStore<byte[]> articleStore;
@@ -60,8 +67,99 @@ namespace osu.Framework.Tests.Visual
             AddStep(name, () =>
             {
                 var bytes = articleStore.Get($"{name}.md");
-                markdownContainer.Text = Encoding.UTF8.GetString(bytes);
+                var str = Encoding.UTF8.GetString(bytes);
+
+                userInputBox.Text = str;
+                markdownBox.Text = str;
             });
+        }
+
+        private abstract class DisplayBox : Container
+        {
+            public abstract string Text { set; }
+            protected abstract string Description { get; }
+
+            private ScrollContainer content;
+            protected override Container<Drawable> Content => content;
+
+            public DisplayBox()
+            {
+                InternalChildren = new[]
+                {
+                    new Container
+                    {
+                        Name = "Header",
+                        RelativeSizeAxes = Axes.X,
+                        Height = 20,
+                        Children = new Drawable[]
+                        {
+                            new Box
+                            {
+                                Colour = Color4.Gold,
+                                RelativeSizeAxes = Axes.Both,
+                            },
+                            new SpriteText
+                            {
+                                Anchor = Anchor.Centre,
+                                Origin = Anchor.Centre,
+                                Text = Description,
+                                Colour = Color4.Black
+                            }
+                        }
+                    },
+                    new Container
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                        Padding = new MarginPadding { Top = 20 },
+                        Children = new Drawable[]
+                        {
+                            new Box
+                            {
+                                Name = "Background",
+                                RelativeSizeAxes = Axes.Both,
+                                Colour = new Color4(50, 50, 50, 255)
+                            },
+                            content = new ScrollContainer { RelativeSizeAxes = Axes.Both }
+                        }
+                    }
+                };
+            }
+        }
+
+        private class MarkdownBox : DisplayBox
+        {
+            protected override string Description => "Markdown";
+
+            private readonly MarkdownTextContainer markdownContainer;
+
+            public MarkdownBox()
+            {
+                Add(markdownContainer = new MarkdownTextContainer
+                {
+                    RelativeSizeAxes = Axes.X,
+                    AutoSizeAxes = Axes.Y
+                });
+            }
+
+            public override string Text { set => markdownContainer.Text = value; }
+        }
+
+        private class UserTextBox : DisplayBox
+        {
+            protected override string Description => "Raw";
+
+            private readonly TextFlowContainer textContainer;
+
+            public UserTextBox()
+            {
+                Add(textContainer = new TextFlowContainer
+                {
+                    RelativeSizeAxes = Axes.X,
+                    AutoSizeAxes = Axes.Y
+                });
+            }
+
+            public override string Text { set => textContainer.Text = value; }
         }
     }
 }
